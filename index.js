fedb38f3c012e5402c4309bc543b9fdb15fd0f84
Amirouchissam <amirouchissam3@gmail.com>
2026-05-16 16:32:44 +0100

    gg

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const button = document.getElementById("btn");
 
+const savedusername = JSON.parse(localStorage.getItem("username") || "null");
+if (savedusername) {
+    nom.value = savedusername || "";
+    email.value = savedusername || "";
+    password.value = savedusername || "";
+}
+
+button.addEventListener("click", function(event){
+    event.preventDefault();
+
+    const user ={
+        username: nom.value,
+        email: email.value,
+        password: password.value
+    };
+    localStorage.setItem("username", JSON.stringify(user));
+    alert("Name: " + nom.value + "\nEmail: " + email.value + "\nPassword: " + password.value);
+});
 button.addEventListener("click", (event) => {
 
     event.preventDefault();
